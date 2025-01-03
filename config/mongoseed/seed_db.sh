31a00f9166ab736f29ddf3b4da894714b3e93b0d
Nick Brimmer <nickbrimmer@gmail.com>
2025-01-03 12:10:16 -0800

    WA-002-no-double-database

--- config/mongoseed/seed_db.sh
+++ config/mongoseed/seed_db.sh
@@ -2,4 +2,4 @@ sleep 2
 echo "=========> Inserting Workout Test Data"
 mongoimport --host mongodb --db "workouts-database" --collection workouts --file ./workout.json --jsonArray
 
-mongosh mongodb:27017/workoutDb ./create_indices.js
+mongosh mongodb:27017/workouts-database ./create_indices.js
